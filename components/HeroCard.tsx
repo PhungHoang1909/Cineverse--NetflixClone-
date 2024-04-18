29823b1e5554412749e523b0838300e073987e6b
phung hoang <lnphunghoang190903@gmail.com>
2024-04-18 09:09:02 +0700

    Add hero banner for trending movies

---
+++ components/HeroCard.tsx
@@ -0,0 +1,28 @@
+import { baseImgUrl } from "@lib/constants"
+import { Movie } from "@lib/types"
+import { InfoOutlined, PlayCircleOutlineOutlined } from "@mui/icons-material";
+
+const HeroCard = ({ trendingMovie }: { trendingMovie: Movie}) => {
+    console.log(trendingMovie);
+  return (
+    <div className="hero">
+        <div className="hero-bg">
+            <img
+                src={`${baseImgUrl}${trendingMovie?.backdrop_path || trendingMovie?.poster_path}`}
+                alt="trending movie"
+                className="hero-bg-img" 
+            />
+            <div className="overlay"></div>
+        </div>
+
+        <h1 className="hero-title">{trendingMovie?.title || trendingMovie?.name}</h1>
+        <p className="hero-overview">{trendingMovie?.overview}</p>   
+        <div className="hero-btns">
+            <button><PlayCircleOutlineOutlined/> Play Now</button>
+            <button><InfoOutlined/> More info</button>
+        </div>
+    </div>
+  )
+}
+
+export default HeroCard
